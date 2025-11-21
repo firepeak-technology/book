dda1312119796ad2abcc9423233d2814d70a696c
Bo Vandersteene <bo@firepeak.tech>
2025-11-21 21:32:51 +0100

    feat: make pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,37 +1,64 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import tailwindcss from "@tailwindcss/vite";
+import {VitePWA} from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [ tailwindcss(),vue(),],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    host: '0.0.0.0', // Allow access from outside container
-    port: 5173,
-    // Proxy API requests during development
-    proxy: {
-      '/api': {
-        target: 'http://backend:3000/api',
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    // Optimize chunks
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vue-vendor': ['vue', 'vue-router', 'pinia'],
-          'ui-vendor': ['html5-qrcode'],
+    plugins: [tailwindcss(), vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'icons/*.png'],
+            manifest: {
+                "name": "Parrot Library",
+                "short_name": "Books",
+                "description": "My personal book collection manager",
+                "start_url": "/",
+                "display": "standalone",
+                "background_color": "#ffffff",
+                "theme_color": "#9E1E18",
+                "orientation": "portrait",
+                icons: [
+                    {
+                        src: '/icons/icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/icons/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
         },
-      },
     },
-  },
+    server: {
+        host: '0.0.0.0', // Allow access from outside container
+        port: 5173,
+        // Proxy API requests during development
+        proxy: {
+            '/api': {
+                target: 'http://backend:3000/api',
+                changeOrigin: true,
+            },
+        },
+    },
+    build: {
+        outDir: 'dist',
+        sourcemap: false,
+        // Optimize chunks
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'vue-vendor': ['vue', 'vue-router', 'pinia'],
+                    'ui-vendor': ['html5-qrcode'],
+                },
+            },
+        },
+    },
 })
